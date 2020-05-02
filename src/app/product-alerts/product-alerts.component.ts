d5d9e4839d0b0d57d21a95dddbd227d6dc4cfdba
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2020-05-02 10:29:05 +0600

    added angular forms

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductAlertsComponent {
+  @Input() product;
+  @Output() notify = new EventEmitter();
 }
